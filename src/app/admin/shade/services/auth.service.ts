bbe59f06419e4b70c5ccb0818b17f97f70ece60b
Marina-H-S <hykavchuk.marina@gmail.com>
2020-03-17 17:25:51 +0200

    Added storege token

--- src/app/admin/shade/services/auth.service.ts
+++ src/app/admin/shade/services/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { User } from 'src/app/shared/interfaces';
-import { Observable } from 'rxjs';
+import { User, FbAuthResponse } from 'src/app/shared/interfaces';
+import { Observable, from } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { tap } from 'rxjs/operators';
 
 
 @Injectable()
@@ -12,22 +14,37 @@ export class AuthService {
     }
 
     get token(): string {
-        return '';
+        const expDate = new Date(localStorage.getItem('fb-token-exp'));
+        if (new Date() > expDate) {
+            this.logout();
+            return null;
+        }
+        return localStorage.getItem('fb-token');
     }
 
     login(user: User): Observable<any> {
-       return this.http.post('', user);
+        user.returnSecureToken = true;
+        return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user).pipe(
+           tap(this.setToken)
+       );
     }
 
     logout() {
-
+        this.setToken(null);
     }
 
     isAuthenticated(): boolean {
         return !!this.token;
     }
 
-    private setToken() {
+    private setToken(response: FbAuthResponse | null) {
+        if (response) {
+            const expDate = new Date(new Date().getTime() + +response.expiresIn * 1000);
+            localStorage.setItem('fb-token', response.idToken);
+            localStorage.setItem('fb-token-exp', expDate.toString());
+        } else {
+            localStorage.clear();
+        }
 
     }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import { Enviroment } from './interface';
+
+export const environment: Enviroment = {
+  production: true,
+  apiKey: 'AIzaSyB0i5ck3dmS96n92NDB9vfRD-HOoLV7knQ'
 };
