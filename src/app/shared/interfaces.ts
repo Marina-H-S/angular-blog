61cbd51f3205e2acc1ff03dcc8cd92821196916c
Marina-H-S <hykavchuk.marina@gmail.com>
2020-03-18 10:05:19 +0200

    Added form for creating post

--- src/app/shared/interfaces.ts
+++ src/app/shared/interfaces.ts
@@ -8,3 +8,10 @@ export interface FbAuthResponse {
     idToken: string;
     expiresIn: string;
 }
+export interface Post {
+    id?: string;
+    title: string;
+    text: string;
+    author: string;
+    date: Date;
+}
