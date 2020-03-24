02d8051b7196798002f8527a89b011267a9148a0
Marina-H-S <hykavchuk.marina@gmail.com>
2020-03-24 16:08:58 +0200

    Added filter pipe

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -9,9 +9,10 @@ import { EditPageComponent } from './edit-page/edit-page.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 import { AuthGaurd } from './shade/services/auth.guard';
+import { SearchPipe } from './shade/search.pipe';
 
 @NgModule({
-    declarations: [AdminLayoutComponent, LoginPageComponent, DashboardPageComponent, CreatePageComponent, EditPageComponent],
+    declarations: [AdminLayoutComponent, LoginPageComponent, DashboardPageComponent, CreatePageComponent, EditPageComponent, SearchPipe],
     imports: [
         CommonModule,
         FormsModule,
